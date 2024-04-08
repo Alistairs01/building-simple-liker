9aa580fcbf7d75382008ce5049efad8e2d6687b7
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-08 12:11:44 +0300

    building simple liker

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// grab the element
+let simpleLikes = document.querySelectorAll(".like-glyph");
+// iterating over the hearts using forEach
+simpleLikes.forEach(setLike => {
+  // set hearts that are supposed to be empty
+  setLike.textContent = EMPTY_HEART;
+// invoke the server callback
+  mimicServerCall().then(() => {
 
+  })
+  .catch((error) => {
+    let errorShow = document.querySelector("#modal-message");
+    errorShow.textContent = error;
+  });
+  // create the event listener
+  setLike.onclick = (e) => {
+    if (e.target.textContent === EMPTY_HEART) {
+      e.target.textContent = FULL_HEART
+      e.target.classList.add("activated-heart");
+    }else {
+      e.target.textContent = EMPTY_HEART
+      e.target.classList.remove("activated-heart");
+    }
+    // grab the element with for hiding 
+    let hidingModal = document.querySelector(".hidden");
+    // remove the class name hidden 
+    hidingModal.classList.remove("hidden");
+    // add back the hidden modal after 3 seconds 
+     setTimeout(() => {
+      hidingModal.classList.add("hidden");
+     },3000);
+  };
+});
 
 
 
